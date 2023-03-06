083e10fbfd4d18eb32cb205ccb08487d8e94fb92
ChuanZhi Fang <52807202+B612plant@users.noreply.github.com>
2023-03-06 10:32:45 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zhiri0930/my_VuePress_Blog.git master:feature/zhiri-pages
+git push -f git@github.com:B612planet/my_VuePress_Blog.git master:feature/zhiri-pages
 
 cd -
